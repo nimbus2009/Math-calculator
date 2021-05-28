4558e1af8534a549ccc801bd0831559b7ae40aad
Anuj Doddakaragi <83464826+nimbus2009@users.noreply.github.com>
2021-05-28 16:20:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,18 +9,30 @@ function setup() {
   num2=createInput();
   num1.position(5,60);
   num2.position(5,90);
+  num1.style("background-color: PaleGoldenRod;color: black");
+  num2.style("background-color: PaleGoldenRod;color: black");
+  num1.size(150,15);
+  num2.size(150,15);
 
   add=createButton("Add");
   add.position(5,120);
+  add.style("background-color: aqua;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)");
+  add.size(70,20);
 
   subtract=createButton("Subtract");
   subtract.position(90,120);
+  subtract.style("background-color: GreenYellow;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)");
+  subtract.size(70,20);
 
   multiply=createButton("Multiply");
   multiply.position(5,160);
+  multiply.style("background-color: coral;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);")
+  multiply.size(70,20);
 
   divide=createButton("Divide");
   divide.position(90,160);
+  divide.style("background-color: plum;box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);");
+  divide.size(70,20);
 
   add.mousePressed(addition);
   subtract.mousePressed(sub);
@@ -33,7 +45,10 @@ function draw() {
   n1=parseInt(num1.value());
   n2=parseInt(num2.value());
 
-
+  fill("maroon");
+  stroke("maroon");
+  strokeWeight(0.5);
+  textSize(15);
   text("Answer- " + answer,10,220);
 
 }
